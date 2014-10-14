1c9418738477357d641ef897dee9e110d59de5de
Marcello Angileri <marcelloangileri@gmail.com>
2014-10-14 14:03:07 +0200

    Multiple markers
    Signed-off-by: Marcello Angileri <marcelloangileri@gmail.com>

--- MapOptions/js/main.js
+++ MapOptions/js/main.js
@@ -12,13 +12,29 @@
 		//create the map
 		var map = new google.maps.Map(document.getElementById('map'), options);
 		
-		//adding Marker
+		// ------------- adding Marker ---------------------------
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(40.7257, -74.0047),
-			map: map
+			map: map,
+			//------------- Adding a tooltip --------------------
+			title: "This is a tooltip."
+			//------------- Changing the icon -------------------
+			//icon: "https://gmaps-samples.googlecode.com/svn/trunk/markers/blue/blank.png"
 		});
-
-	
+		
+		//--------------- create an infoWindow -------------------
+		var infowindow = new google.maps.InfoWindow({
+			content: '<div class="infoWindow">This is an infoWindow</div>'
+		});
+		
+		//assign hte infoWindow to a specific Marked using eventListener
+		google.maps.event.addListener(
+		marker,
+		'click',
+		function(){
+			//here we open the infoWindow
+			infowindow.open(map, marker);
+		});
 	};
 	
 })();
