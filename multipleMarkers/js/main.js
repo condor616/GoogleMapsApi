7f2b47740c310043348d0c95f449c175dd8999f4
Marcello Angileri <marcelloangileri@gmail.com>
2014-10-14 16:03:38 +0200

    Errors
    Signed-off-by: Marcello Angileri <marcelloangileri@gmail.com>

--- multipleMarkers/js/main.js
+++ multipleMarkers/js/main.js
@@ -12,26 +12,54 @@
 		//create the map
 		var map = new google.maps.Map(document.getElementById('map'), options);
 		
-		//adding click events to the buttons
+		//create an array that will contains the coordinates
+		//for New York, San Francisco and Seattle
+		var places = [];
 		
-		//getting values
-		document.getElementById('getValues').onclick = function(){
-			alert('Current Zoom level is ' + map.getZoom());
-			alert('Current center is ' + map.getCenter());
-			alert('Current MapType is ' + map.getMapTypeId());
+		places.push(new google.maps.LatLng(40.756, -73.986)); //NewYork
+		places.push(new google.maps.LatLng(37.775, -122.419)); //San Francisco
+		places.push(new google.maps.LatLng(47.620, -122.347)); //Seattle
+		
+		//we only need one infoWindow, so we declare it here
+		var infowindow;
+		
+		//Let's go through the array and extract the data
+		for (var i=0; i<places.length; i++){
+		
+			//create a new Marker
+			var marker = new google.maps.Marker({
+				position: places[i],
+				map: map,
+				title: 'Place number ' + i
+			});
+			
+			//Wrapping the event listener inside an anonymous function
+			//that we immetially invoke and passes the variable i to
+			(function(i, marker){
+				
+				//create the event listener
+				google.maps.event.addListener(
+					marker,
+					'click',
+					function(){
+						
+						if (!infowindow){
+							infowindow = new google.maps.InfoWindow();
+						}
+						
+						infowindow.setContent('<div class=\"infoWindow\">Place number '+i+'</div>');
+						
+						
+						//open the infoWindow
+						infowindow.open(map, marker);
+					}
+				);
+				
+			})(i, marker);
+		
 		}
 		
-		//changing values
-		document.getElementById('changeValues').onclick = function(){
-			map.setOptions(
-			{
-				center: new google.maps.LatLng(40.6891, -74.0445),
-				zoom: 17,
-				mapTypeId: google.maps.MapTypeId.SATELLITE
-			}
-			);
-		}
 	
-	};
+	}
 	
 })();
