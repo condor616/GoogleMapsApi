505f44d0493893e94359b848b430a0e85efe85a5
Marcello Angileri <marcelloangileri@gmail.com>
2014-10-14 13:40:10 +0200

    Setting starting point
    Signed-off-by: Marcello Angileri <marcelloangileri@gmail.com>

--- MapOptions/js/main.js
+++ MapOptions/js/main.js
@@ -4,33 +4,20 @@
 	
 		//create MapOptions object
 		var options = {
-			zoom: 3,
-			center:  new google.maps.LatLng(37.09, -95.71),
+			zoom: 12,
+			center:  new google.maps.LatLng(40.7257, -74.0047),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		
 		//create the map
 		var map = new google.maps.Map(document.getElementById('map'), options);
 		
-		//adding click events to the buttons
-		
-		//getting values
-		document.getElementById('getValues').onclick = function(){
-			alert('Current Zoom level is ' + map.getZoom());
-			alert('Current center is ' + map.getCenter());
-			alert('Current MapType is ' + map.getMapTypeId());
-		}
-		
-		//changing values
-		document.getElementById('changeValues').onclick = function(){
-			map.setOptions(
-			{
-				center: new google.maps.LatLng(40.6891, -74.0445),
-				zoom: 17,
-				mapTypeId: google.maps.MapTypeId.SATELLITE
-			}
-			);
-		}
+		//adding Marker
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(40.7257, -74.0047),
+			map: map
+		});
+
 	
 	};
 	
